002bf63263cb4808af27092deac6e28082bf2d3c
harmansethi92 <harman@peeriq.com>
2018-11-18 11:35:17 -0500

    service2

---
+++ casestudy1/service2/index.php
@@ -0,0 +1,15 @@
+<h1>Hello REORG TEAM!</h1>
+<h4>Attempting MySQL connection from php service2...</h4>
+<?php
+
+$MYSQL_USER= getenv('MYSQL_USER');
+$MYSQL_PASSWORD=getenv('MYSQL_PASSWORD');
+
+$db = new PDO('mysql:host=mysql;port=3306;dbname=test_db', $MYSQL_USER, $MYSQL_PASSWORD);
+
+if ($db->connect_error) {
+	    die("Connection failed: " . $db->connect_error);
+}
+echo "Connected to MySQL successfully!";
+?>
+
